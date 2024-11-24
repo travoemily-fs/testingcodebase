c183d086ea5d7eb27dd47ffd2dc4868af2e5d7d2
travoemily-fs <EATravo@student.fullsail.edu>
2024-11-24 11:41:12 -0800

    final repo update
    update to the repo after making final changes to project. check CHANGELOG.md for more info!

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -2,6 +2,17 @@
 
 > All relevant project updates will be logged here.
 
+**LIVE DEMO ENVIRONMENTS**
+
+> I created a collection on CodePen of each function that I used for fiddling with numbers and ensuring that the functions were performing as expected.
+
+**_CodePen collection:_** [x](https://codepen.io/collection/MWKVOJ)
+
+- **Function #1:** - [x](https://codepen.io/travoemily-fs/pen/XWvvEEb)
+- **Function #2:** - [x](https://codepen.io/travoemily-fs/pen/ZEggZyp)
+- **Function #3:** - [x](https://codepen.io/travoemily-fs/pen/yLmmWgP)
+- **Function #4:** - [x](https://codepen.io/travoemily-fs/pen/OJKKYEW)
+
 **ADDED:**
 
 - **11/18** - initial repo creation
@@ -21,17 +32,29 @@
 - **11/21** - finalized structuring of each section + inputs needed to set up js and further testing in console
 - **11/22** - finalized on project color palette
 - **11/22** - completed the visual aspect of the project
+- **11/24** - compiled codes from codepen demos into main file
 
 **CHANGED:**
 
 - **11/22** - altered the functionality of function #4 to a user inputted string of text rather than a pre-generated string (for testability)
+- **11/22** - changed form id to form names to target forms using the form parameter in js
+- **11/24** - modified error alerts from codepen to include tabler icons
 
 **FIXED:**
 
-- n/a
+- **11/24**- changed ts configuration lib and target to es2016 to avoid 'includes' error throwing
+- **11/24** - fixed the 'null' error catching on DOM elements
+- **11/24** - fixed error caught when adding a space between first and last name on function #3 (in case users want to enter both, i don't want an error to throw)
+- **11/24** - fixed an error where inputs don't clear after error is caught
 
 **REMOVED:**
 
 - n/a
 
 **RESOURCES USED:**
+
+- **using the 'every' array method:** [x](https://www.w3schools.com/jsref/jsref_every.asp)
+- **using 'test' regex method:** [x](https://www.w3schools.com/jsref/jsref_regexp_test.asp)
+- **regex patterns and sequences:** [x](https://medium.com/@siddharthgov01/regular-expressions-from-a-za-z-88cf9cf0abac)
+- **regex number expressions:** [x](https://www.freecodecamp.org/news/what-does-d-mean-in-regex/#:~:text=Conclusion-,What%20is%20%5Cd%20in%20RegEx%3F,digit%20from%200%20to%209.)
+- **reversing strings in javascript:** [x](https://www.programiz.com/javascript/examples/reverse-string)
--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,168 @@
+// function test 1: sumOfarray(arr)
+const s1Submit = document.getElementById("s1Btn") as HTMLButtonElement;
+const s1Results = document.querySelector(".s1Results") as HTMLElement;
+const s1Input = document.getElementById("s1Input") as HTMLInputElement;
+
+const s1Nums = (event: Event): void => {
+  event.preventDefault();
+
+  const input1 = s1Input.value;
+  const numArray = input1.split(",").map((num) => num.trim());
+
+  if (!numArray.every((num) => !isNaN(Number(num)) && num !== "")) {
+    s1Input.value = "";
+    s1Results.style.opacity = "1";
+    s1Results.innerHTML = `<i class="ti ti-alert-circle"></i> <span>ERROR:</span> please enter valid whole numbers separated by commas.`;
+    return;
+  }
+
+  if (numArray.length !== 4) {
+    s1Input.value = "";
+    s1Results.style.opacity = "1";
+    s1Results.innerHTML = `<i class="ti ti-alert-circle"></i> <span>ERROR:</span> exactly 4 numbers are required to proceed.`;
+    return;
+  }
+
+  let sum = 0;
+  numArray.forEach((num) => {
+    sum += Number(num);
+  });
+
+  s1Results.style.opacity = "1";
+  s1Results.innerHTML = `<i class="ti ti-calculator"></i> the sum of the array is: <br> <b>${sum}</b>`;
+  s1Input.value = "";
+};
+
+// function test 2: findMax(arr)
+const s2Submit = document.getElementById("s2Btn") as HTMLButtonElement;
+const s2Results = document.querySelector(".s2Results") as HTMLElement;
+const s2Input = document.getElementById("s2Input") as HTMLInputElement;
+
+const s2Nums = (event: Event): void => {
+  event.preventDefault();
+  const input2 = s2Input.value;
+  const numArray2 = input2.split(",").map((num) => num.trim());
+
+  if (!numArray2.every((num) => !isNaN(Number(num)) && num !== "")) {
+    s2Input.value = ""; //
+    s2Results.style.opacity = "1";
+    s2Results.innerHTML = `<i class="ti ti-alert-circle"></i> <span>ERROR:</span> please enter valid whole numbers, separated by commas.`;
+    return;
+  }
+
+  if (numArray2.length !== 4) {
+    s2Input.value = ""; //
+    s2Results.style.opacity = "1";
+    s2Results.innerHTML = `<i class="ti ti-alert-circle"></i> <span>ERROR:</span> exactly 4 whole numbers are required to proceed.`;
+    return;
+  }
+
+  const maxNumArray = numArray2.map(Number);
+  const max = Math.max(...maxNumArray);
+  s2Results.style.opacity = "1";
+  s2Results.innerHTML = `<i class="ti ti-calculator"></i> the largest number in the array is: <br>
+  <b>${max}</b>`;
+  s2Input.value = "";
+};
+
+// function test 3: countVowels(str)
+const s3Submit = document.getElementById("s3Btn") as HTMLButtonElement;
+const s3Results = document.querySelector(".s3Results") as HTMLElement;
+const s3Input = document.getElementById("s3Input") as HTMLInputElement;
+
+const s3Vowels = (event: Event): void => {
+  event.preventDefault();
+  const input3 = s3Input.value.trim();
+
+  if (input3 === "") {
+    s3Input.value = "";
+    s3Results.style.opacity = "1";
+    s3Results.innerHTML = `<i class="ti ti-alert-circle"></i> <span>ERROR:</span> you must enter your name to proceed`;
+    return;
+  }
+
+  if (/\d/.test(input3)) {
+    s3Input.value = "";
+    s3Results.style.opacity = "1";
+    s3Results.innerHTML = `<i class="ti ti-alert-circle"></i> <span>ERROR:</span> your name cannot include numbers. please try again!`;
+    return;
+  }
+
+  if (!/^[a-zA-Z\s]+$/.test(input3)) {
+    s3Input.value = "";
+    s3Results.style.opacity = "1";
+    s3Results.innerHTML = `<i class="ti ti-alert-circle"></i> <span>ERROR:</span> your name can only contain letters. please remove any special characters and try again.`;
+    return;
+  }
+  const vowels: string[] = ["a", "e", "i", "o", "u", "y"];
+  let vowelCount = 0;
+  for (let char of input3.toLowerCase()) {
+    if (vowels.includes(char)) {
+      vowelCount++;
+    }
+  }
+
+  s3Results.style.opacity = "1";
+  s3Results.innerHTML = `<i class="ti ti-confetti"></i> your name, <b>${input3}</b>, contains <b>${vowelCount}</b> vowels. neat!`;
+  s3Input.value = "";
+};
+
+// function test 4: reverseString(str)
+const s4Submit = document.getElementById("s4Btn") as HTMLButtonElement;
+const s4Results = document.querySelector(".s4Results") as HTMLElement;
+const s4Input = document.getElementById("s4Input") as HTMLInputElement;
+
+const s4String = (event: Event): void => {
+  event.preventDefault();
+
+  const input4 = s4Input.value.trim();
+
+  if (input4 === "") {
+    s4Input.value = ""; //
+    s4Results.style.opacity = "1";
+    s4Results.innerHTML = `<i class="ti ti-alert-circle"></i> <span>ERROR:</span> please enter a sentence to reverse.`;
+    return;
+  }
+
+  if (/\d/.test(input4)) {
+    s4Input.value = ""; //
+    s4Results.style.opacity = "1";
+    s4Results.innerHTML = `<i class="ti ti-alert-circle"></i> <span>ERROR:</span> your sentence cannot include numbers. Please try again!`;
+    return;
+  }
+
+  if (/^[.,!?'\s]+$/.test(input4)) {
+    s4Input.value = "";
+    s4Results.style.opacity = "1";
+    s4Results.innerHTML = `<i class="ti ti-alert-circle"></i> <span>ERROR:</span> your input cannot consist only of punctuation or spaces. please enter a valid sentence.`;
+    return;
+  }
+
+  if (!/^[a-zA-Z\s.,!?']+$/.test(input4)) {
+    s4Input.value = "";
+    s4Results.style.opacity = "1";
+    s4Results.innerHTML = `<i class="ti ti-alert-circle"></i> <span>ERROR:</span> your sentence can only contain letters, spaces, and basic punctuation (, . ! ? '). please try again.`;
+    return;
+  }
+
+  let reversedString = "";
+  for (let i = input4.length - 1; i >= 0; i--) {
+    reversedString += input4[i];
+  }
+
+  s4Results.style.opacity = "1";
+  s4Results.innerHTML = `<i class="ti ti-confetti"></i> here is your sentence reversed: <br>
+  ${reversedString}`;
+  s4Input.value = "";
+};
+
 (() => {
-    console.log("welcome to the console of my codebase!");
-    
-    // function test 1: sumOfarray(arr)
-
-    // function test 2: findMax(arr)
-
-    // function test 3: countVowels(str)
-
-    // function test 4: reverseString(stri)
-  })();
+  // event listener for function #1 btn click
+  s1Submit.addEventListener("click", s1Nums);
+  // event listener for function #2 btn click
+  s2Submit.addEventListener("click", s2Nums);
+  // event listener for function #3 btn click
+  s3Submit.addEventListener("click", s3Vowels);
+  // event listener for function #4 btn click
+  s4Submit.addEventListener("click", s4String);
+})();
